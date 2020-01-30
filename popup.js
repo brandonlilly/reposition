694a6b8e6c67cff23afc75b7880262796cbf51b1
Brandon Lilly <brandonjlilly@gmail.com>
2020-01-30 13:18:37 -0600

    ignore minimized and maximized windows

--- popup.js
+++ popup.js
@@ -5,22 +5,23 @@ document.addEventListener('DOMContentLoaded', function() {
   };
 
   chrome.windows.getAll(null, function(windows) {
-    var targetWindow = windows[0];
+    var ignoredStates = ['maximized', 'fullscreen', 'minimized'];
+
+    var filtered = windows.filter(function(window) {
+      return !ignoredStates.includes(window.state);
+    });
+
+    var targetWindow = filtered[0];
 
     // target window first, normal, then incognito, then minimized windows last
-    var sorted = windows.sort(function(window) {
+    var sorted = filtered.sort(function(window) {
       if (window.id == targetWindow.id) return -1;
       if (window.incognito === true) return 1;
       if (window.state === 'minimized') return 2;
       return 0;
     });
 
-    var ignoredStates = ['maximized', 'fullscreen'];
-    var sortedAndfiltered = sorted.filter(function(window) {
-      return !ignoredStates.includes(window.state);
-    });
-
-    sortedAndfiltered.forEach(function(window, index) {
+    sorted.forEach(function(window, index) {
       chrome.windows.update(window.id, {
         top:  targetWindow.top + offset.y * index,
         left: targetWindow.left + offset.x * index,
